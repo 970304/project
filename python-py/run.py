a0441e009424befc1293fec31bc4cf66dfe17128
970304 <1731083654@qq.com>
2020-09-02 17:19:20 +0800

    2

--- python-py/run.py
+++ python-py/run.py
@@ -1,6 +1,6 @@
 import pytest
 
 
-pytest.main(["--alluredir=outfile/allure", "--junit-xml=outfile/report/a.xml"])
+pytest.main(["--alluredir=./outfile/allure", "--junit-xml=./outfile/report/a.xml"])
 
 
